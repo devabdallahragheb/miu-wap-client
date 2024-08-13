1bfd3ed3cc8ec2764fcf75aba2155b21f0eddd07
aragheb <abdallah.ragheb@squadio.com>
2024-08-13 06:56:30 +0300

    add google auth

---
+++ src/components/QAuth.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+export default function QAuth() {
+  const dispatch = useDispatch();
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+      const result = await signInWithPopup(auth, provider);
+
+      console.log(result);
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          photo: result.user.photoURL,
+          email: result.user.email,
+        }),
+      });
+
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <button
+        onClick={handleGoogleClick}
+        className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
+      >
+        {" "}
+        Containue With Google
+      </button>
+    </div>
+  );
+}
--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -6,6 +6,7 @@ import {
   signInSuccess,
   signInFailure,
 } from "../redux/user/userSlice";
+import QAuth from "../components/QAuth";
 export default function Signin() {
   const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
@@ -67,6 +68,7 @@ export default function Signin() {
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
+        <QAuth></QAuth>
       </form>
       <div className="flex gap-2 mt-5">
         <p> Dont Have an account ?</p>

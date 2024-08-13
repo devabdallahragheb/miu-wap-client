30b4d3ae5a56e253382a2091d39c21beeff4a14b
aragheb <abdallah.ragheb@squadio.com>
2024-08-14 00:41:47 +0300

    add listing and create listing

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
 import PrivateRoute from "./components/PrivateRoute";
 import Header from "./components/Header";
+import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 export default function App() {
   return (
     <BrowserRouter>
-    <Header></Header>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
-        <Route element={<PrivateRoute />} >
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Route>
+        <Route path="/listing/:listingId" element={<Listing />} />
       </Routes>
     </BrowserRouter>
   );
